dc71d0b9ccb67c2fb6200d9e826ed1890bd28d02
kalu661 <lukaslopez49@gmail.com>
2022-05-05 14:37:59 -0300

    mdsc

---
+++ Modelo Secuencial/secu.js
@@ -0,0 +1,21 @@
+async function funcionLineal() {
+	let num = parseInt(document.getElementById("number").value);
+	//modelo secuencial
+
+	const modelo = tf.sequential();
+	//se agrega una capa
+	modelo.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+	modelo.compile({
+		loss: "meanSquaredError",
+		optimizer: "sgd",
+	});
+	const xs = tf.tensor2d([1, 3, 5], [3, 1]);
+	const ys = tf.tensor2d([2, 6, 10], [3, 1]);
+
+	await modelo.fit(xs, ys, { epochs: 800 });
+
+	document.getElementById("salida").innerHTML = modelo.predict(
+		tf.tensor2d([num], [1, 1])
+	);
+}
