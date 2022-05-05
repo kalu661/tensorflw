c7a0fd3de4f8ee6cd88959151956716826b6d563
kalu661 <lukaslopez49@gmail.com>
2022-05-05 13:50:22 -0300

    mn

--- Tensores Regresión Lineal/regres.js
+++ Tensores Regresión Lineal/regres.js
@@ -2,28 +2,27 @@ let x_pos = [];
 let y_pos = [];
 
 let w, b;
-const learningRate = 0.6;
 
+const learningRate = 0.5;
 const optimizer = tf.train.sgd(learningRate);
 
 function setup() {
-	createCanvas(1720, 720);
+	createCanvas(800, 600);
 	background(0);
-	w = tf.variable(tf.scalar(random(20)));
-	b = tf.variable(tf.scalar(random(20)));
+	w = tf.variable(tf.scalar(random(1)));
+	b = tf.variable(tf.scalar(random(1)));
 }
 
 function predecir(x) {
 	const xs = tf.tensor1d(x);
+	// y=w*x + b
 	const ys = xs.mul(w).add(b);
-
 	return ys;
 }
 
 function mousePressed() {
 	let x = map(mouseX, 0, width, 0, 1);
 	let y = map(mouseY, 0, height, 1, 0);
-
 	x_pos.push(x);
 	y_pos.push(y);
 }
@@ -31,7 +30,7 @@ function mousePressed() {
 function draw() {
 	background(0);
 	stroke(255);
-	strokeWeight(8);
+	strokeWeight(10);
 	for (i = 0; i < x_pos.length; i++) {
 		let px = map(x_pos[i], 0, 1, 0, width);
 		let py = map(y_pos[i], 0, 1, height, 0);
@@ -43,15 +42,12 @@ function draw() {
 			optimizer.minimize(() => predecir(x_pos).sub(ys).square().mean());
 		}
 	});
-
 	let y_pred = tf.tidy(() => predecir([0, 1]));
 	let val_y_pred = y_pred.dataSync();
 	y_pred.dispose();
-
 	let x1 = map(0, 0, 1, 0, width);
 	let x2 = map(1, 0, 1, 0, width);
-
 	let y1 = map(val_y_pred[0], 0, 1, height, 0);
 	let y2 = map(val_y_pred[1], 0, 1, height, 0);
-
 	line(x1, y1, x2, y2);
+}
