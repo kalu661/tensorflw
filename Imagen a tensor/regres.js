a340ca1d20d41ba7364875d59e6afed7e44241bb
kalu661 <lukaslopez49@gmail.com>
2022-05-06 09:55:44 -0300

    img_to_tensor

---
+++ Imagen a tensor/regres.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+	tf.tidy(() => {
+		// Simply read from the DOM
+		const gantImage = document.getElementById("gant");
+		const gantTensor = tf.browser.fromPixels(gantImage);
+		console.log(
+			`Successful conversion from DOM to a ${gantTensor.shape} tensor`
+		);
+		// Now load an image object in JavaScript
+		const cake = new Image();
+		cake.crossOrigin = "anonymous";
+		// cake.src = "/kalu.jpg";
+		cake.onload = () => {
+			const cakeTensor = tf.browser.fromPixels(cake);
+			document.getElementById("salida").innerHTML = console.log(
+				`Successful conversion from Image() to a ${cakeTensor.shape} tensor`
+			);
+		};
+	});
+});
